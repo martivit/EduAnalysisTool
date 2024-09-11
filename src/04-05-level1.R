8a5bcbf28cd918ba54d753ac7c647f2bf6020601
martivit <martina@10096.it>
2024-09-11 16:29:57 +0200

    test_HTI

--- src/04-05-level1.R
+++ src/04-05-level1.R
@@ -1,10 +1,25 @@
 library(tidyverse)
 library(gt)
 library(openxlsx)
+source("src/functions/create_education_table_group_x_var.R")
+source("src/functions/create_education_xlsx_table.R")
+source ('src/functions/00_edu_helper.R')
+source ('src/functions/00_edu_function.R')
+path_ISCED_file = 'resources/UNESCO ISCED Mappings_MSNAcountries_consolidated.xlsx'
+
 
 # Get results
 education_results_table_labelled <- readRDS("output/labeled_results_table.RDS")
 loa <- readxl::read_excel("input_tool/edu_analysistools_loa.xlsx", sheet = "Sheet1")
+
+
+
+info_country_school_structure <- read_ISCED_info("HTI", path_ISCED_file)
+summary_info_school <- info_country_school_structure$summary_info_school    # DataFrame 1: level code, Learning Level, starting age, duration
+levels_grades_ages <-  info_country_school_structure$levels_grades_ages     # DataFrame 2: level code, Learning Level, Year/Grade, Theoretical Start age, limit age
+
+label_level1 = summary_info_school$name_level[summary_info_school$level_code == "level1"]
+
 
 loa_level1 <- loa %>% 
   mutate(group_var = str_replace_all(group_var, ",", " %/% "),
@@ -16,7 +31,7 @@ education_results_table_labelled <- education_results_table_labelled %>%
 filtered_education_results_table_labelled <- education_results_table_labelled %>% 
   filter(level1, 
          analysis_var_value != "0", 
-         str_detect(group_var_value, "primary education primary school")) |> 
+         str_detect(group_var_value, label_level1)) |> 
   select(-level1)
 
 # gt_helper 
@@ -29,9 +44,9 @@ level1_only <- filtered_education_results_table_labelled %>%
 level1_other <- filtered_education_results_table_labelled %>% 
   filter(!group_var %in% c("edu_school_cycle_d", "edu_school_cycle_d %/% ind_gender")) |> 
   mutate(group_var = str_remove_all(group_var, "edu_school_cycle_d( %/% )*"),
-         group_var_value = str_remove_all(group_var_value, "primary education primary school( %/% )*"), 
+         group_var_value = str_remove_all(group_var_value, paste0(label_level1, "( %/% )*")), 
          label_group_var = str_remove_all(label_group_var, "edu_school_cycle_d( %/% )*"), 
-         label_group_var_value = str_remove_all(label_group_var_value, "primary education primary school( %/% )*"))
+         label_group_var_value = str_remove_all(label_group_var_value, paste0(label_level1, "( %/% )*")))
 all_level1 <- rbind(level1_only, level1_other)
 
 
