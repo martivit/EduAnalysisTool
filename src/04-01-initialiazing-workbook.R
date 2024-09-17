c9ff7545ef4d8e302cb1b58e40afe67bd37d8418
martivit <martina@10096.it>
2024-09-17 08:54:40 +0200

    cleaning un-used function

--- src/04-01-initialiazing-workbook.R
+++
@@ -1,6 +0,0 @@
-library(openxlsx)
-#write ToC
-wb <- openxlsx::createWorkbook("education_results")
-
-addWorksheet(wb, "Table_of_content")
-writeData(wb, sheet = "Table_of_content", x = "Table of Content", startCol = 1, startRow = 1)
