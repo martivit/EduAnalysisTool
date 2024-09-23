5f186d2b6b96ffc968eb97b7121dc8f0c821e6f9
yannsay-impact <79831444+yannsay-impact@users.noreply.github.com>
2024-09-23 14:53:23 +0200

    removing dead code and files

--- MAIN.R
+++ MAIN.R
@@ -2,40 +2,23 @@
 # Load packages -----------------------------------------------------------
 library(impactR.utils)
 library(humind)
-library(humind.data)
 library (presentresults)
+library(analysistools)
 
 # Needed tidyverse packages
 library(dplyr)
 library(readxl)
 library(openxlsx)
-library(writexl)
 library(tidyr)
 library(stringr)
-library(tidyverse)
-library(tibble)
 library(ggplot2)
-library(grid)
-library(gridExtra)
-library(ggtext)
 library(srvyr)
-library(analysistools)
 library(gt)
-
 
 source ('src/functions/00_edu_helper.R')
 source ('src/functions/00_edu_function.R')
 source("src/functions/create_education_table_group_x_var.R")
 source("src/functions/create_education_xlsx_table.R")
-
-# source('src/01-add_education_indicators.R')
-# source('src/02-education_analysis.R')
-# source('src/03-education_labeling.R')
-# source('src/04-02-make-table-disruptions.R')
-# source('src/04-03-make-table-barriers.R')
-# source('src/04-04-make-ece-table.R')
-# source('src/04-05-make-level-table.R')
-
 
 ## --------------------------
 #country_assessment = 'HTI'
@@ -96,7 +79,6 @@ pnta = "pnta"
 dnk = "dnk"
 weight_col = 'weights'
 
-
 # indicators
 ind_access = 'edu_access'
 occupation = 'edu_disrupted_occupation'
@@ -132,7 +114,7 @@ labels_with_ages <- summary_info_school %>%
   mutate(label = extract_label_for_level_ordering(summary_info_school, cur_data())) %>%
   pull(label)
 
-###################################################################################################
+##################################################################################################
 
 # 1 ----------------- 01-add_education_indicators.R ----------------- 
 main_sheet <- label_main_sheet
@@ -145,13 +127,13 @@ add_col5 <- NULL
 add_col6 <- NULL
 add_col7 <- NULL
 add_col8 <- NULL
-# source('src/01-add_education_indicators.R') ## OUTPUT: output/loop_edu_recorded.xlsx
+source('src/01-add_education_indicators.R') ## OUTPUT: output/loop_edu_recorded.xlsx
 
 # 2 ----------------- 02-education_analysis.R ----------------- 
 # source('src/02-education_analysis.R') ## OUTPUT: output/grouped_other_education_results_loop.RDS
 
 # 3 ----------------- 03-education_labeling.R ----------------- 
-# source('src/03-education_labeling.R')  ## OUTPUT: output/labeled_results_table.RDS  ---- df: education_results_table_labelled
+source('src/03-education_labeling.R')  ## OUTPUT: output/labeled_results_table.RDS  ---- df: education_results_table_labelled
 
 # 4 ----------------- create workbook for tables ----------------- 
 wb <- openxlsx::createWorkbook("education_results")
@@ -182,11 +164,9 @@ source('src/04-01-make-table-access-overaged-barriers.R')
 level_table <- "out_of_school"
 source('src/04-01-make-table-access-overaged-barriers.R')
 
-
 # 8 ----------------- 04-04-make-ece-table.R ----------------- 
 level_table = 'ece'
 source('src/04-02-make-level-table.R')
-
 
 # 9 ----------------- 04-05-make-level-table.R ----------------- 
 # To repeat according to the number of levels (except ECE) in the country's school system
@@ -201,30 +181,30 @@ openxlsx::saveWorkbook(wb, "output/education_results.xlsx", overwrite = T)
 # openxlsx::openXL("output/education_results.xlsx")
 
 # 10 ----------------- 05-01-make-level-table.R ----------------- 
-# tab_helper <- "access"
-# results_filtered <- "output/rds_results/access_disruptions_results.rds"
-# source('src/05-01-make-graphs-and-maps-tables.R')
-# 
-# tab_helper <- "overaged"
-# results_filtered <- "output/rds_results/access_overaged_results.rds"
-# source('src/05-01-make-graphs-and-maps-tables.R')
-# 
-# tab_helper <- "out_of_school"
-# results_filtered <- "output/rds_results/barriers_results.rds"
-# source('src/05-01-make-graphs-and-maps-tables.R')
-# 
-# tab_helper <- "ece"
-# results_filtered <- "output/rds_results/ece_results.rds"
-# source('src/05-01-make-graphs-and-maps-tables.R')
-# 
-# tab_helper <- "level1"
-# results_filtered <- "output/rds_results/level1_results.rds"
-# source('src/05-01-make-graphs-and-maps-tables.R')
-# 
-# tab_helper <- "level2"
-# results_filtered <- "output/rds_results/level2_results.rds"
-# source('src/05-01-make-graphs-and-maps-tables.R')
-# 
-# tab_helper <- "level3"
-# results_filtered <- "output/rds_results/level3_results.rds"
-# source('src/05-01-make-graphs-and-maps-tables.R')
+tab_helper <- "access"
+results_filtered <- "output/rds_results/access_results.rds"
+source('src/05-01-make-graphs-and-maps-tables.R')
+
+tab_helper <- "overaged"
+results_filtered <- "output/rds_results/overaged_results.rds"
+source('src/05-01-make-graphs-and-maps-tables.R')
+
+tab_helper <- "out_of_school"
+results_filtered <- "output/rds_results/out_of_school_results.rds"
+source('src/05-01-make-graphs-and-maps-tables.R')
+
+tab_helper <- "ece"
+results_filtered <- "output/rds_results/ece_results.rds"
+source('src/05-01-make-graphs-and-maps-tables.R')
+
+tab_helper <- "level1"
+results_filtered <- "output/rds_results/level1_results.rds"
+source('src/05-01-make-graphs-and-maps-tables.R')
+
+tab_helper <- "level2"
+results_filtered <- "output/rds_results/level2_results.rds"
+source('src/05-01-make-graphs-and-maps-tables.R')
+
+tab_helper <- "level3"
+results_filtered <- "output/rds_results/level3_results.rds"
+source('src/05-01-make-graphs-and-maps-tables.R')
