a7068a98028d615f550b66d08a558f87e1eee26b
yannsay-impact <79831444+yannsay-impact@users.noreply.github.com>
2024-09-24 11:01:50 +0200

    deleting tests folder

--- tests/rds_results.R
+++
@@ -1,9 +0,0 @@
-testthat::test_that("rds filtered results are equal", {
-  expected_output_folder <- "tests/fixtures/rds_results/"
-  actual_output_folder <- "output/rds_results/"
-  expected_output <- list.files(expected_output_folder, full.names = T) |>
-    map(~ readRDS(.x))
-  actual_output <- list.files(actual_output_folder, full.names = T) |>
-    map(~ readRDS(.x))
-  testthat::expect_equal(actual_output, expected_output)
-})
